07ad5c36fb49cfaadaca8a774b3387f91a01eb3a
ufuk <108458376+ufuk-34@users.noreply.github.com>
2022-07-01 12:03:12 +0300

    NotificationListener
    Main sayfasında (page_primary) verilerin sayısının fazla olması dolayısıyla  bölümlere ayrıldı. Liste 10 ar 10 ar gösteriliyor...

--- lib/pages/page_primary.dart
+++ lib/pages/page_primary.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 
 import '../controller/primary_controller.dart';
 import '../element/page_primary_item_widget.dart';
+import '../helper/app_config.dart';
 
 class PagePrimary extends StatefulWidget {
   List? timezoneList;
@@ -16,12 +17,20 @@ class PagePrimary extends StatefulWidget {
 class _PagePrimaryState extends State<PagePrimary> {
   final PrimaryController _con = Get.put(PrimaryController());
 
+
+  @override
+  void initState() {
+    _con.timezoneList=widget.timezoneList;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _con.scaffoldKey,
       appBar: AppBar(
         title: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Column(
               children: [
@@ -38,6 +47,7 @@ class _PagePrimaryState extends State<PagePrimary> {
                     style: Theme.of(context).textTheme.headline2),
               ],
             ),
+            SizedBox(),
             InkWell(
               onTap: () {
                 Get.isDarkMode
@@ -48,13 +58,14 @@ class _PagePrimaryState extends State<PagePrimary> {
                   height: 35,
                   width: 35,
                   decoration: BoxDecoration(
+                      border: Border.all(width: 2,color: AppColors.backgraundLightKoyu),
                       color: Theme.of(context).canvasColor,
                       borderRadius: BorderRadius.circular(18)),
                   child: Icon(
                       Get.isDarkMode
                           ? Icons.light_mode_outlined
                           : Icons.dark_mode_outlined,
-                      color: Theme.of(context).focusColor)),
+                      color: Theme.of(context).focusColor,size: 20)),
             )
           ],
         ),
@@ -65,19 +76,47 @@ class _PagePrimaryState extends State<PagePrimary> {
           borderRadius: BorderRadius.circular(20),
         ),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: List.generate(widget.timezoneList!.length, (index) {
-              return PagePrimaryItemWidget(
-                  timezoneName: widget.timezoneList![index]);
-            }),
+      body: NotificationListener<ScrollEndNotification>(
+        child: Obx(()=>SingleChildScrollView(
+          controller: _con.rrectController,
+          scrollDirection: Axis.vertical,
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: List.generate(_con.gosterimSayisi.value!, (index) {
+                return PagePrimaryItemWidget(
+                    timezoneName: widget.timezoneList![index]);
+              }),
+            ),
           ),
+        )
         ),
+        onNotification: (notification) {
+          print(_con.rrectController.position.extentAfter);
+          if (_con.rrectController.position.extentAfter <
+              1000.0) {
+            _con.pageNo++;
+           print("Timezone listesine 10 tane daha ekle ve listeyi güncelle");
+           if(widget.timezoneList!.length > _con.gosterimSayisi.value!){
+             if(widget.timezoneList!.length - _con.gosterimSayisi.value!<10){
+               _con.gosterimSayisi.value=  _con.gosterimSayisi.value!+widget.timezoneList!.length - _con.gosterimSayisi.value!;
+               _con.gosterimSayisi.refresh();
+               print("${_con.gosterimSayisi} tane gösteriliyor...");
+             }else{
+               _con.gosterimSayisi.value=  _con.gosterimSayisi.value!+10;
+               _con.gosterimSayisi.refresh();
+               print("${_con.gosterimSayisi} tane gösteriliyor...");
+             }
+
+           }else{
+             print("sayfa sonu");
+           }
+
+          }
+          return true;
+        },
       ),
     );
   }
